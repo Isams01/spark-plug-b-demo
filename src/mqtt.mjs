4e8ff28ac68c04b43a71186547fe448199fd6a4c
Isamu Ito <isamu.ito@tokusystems.com>
2024-12-20 14:51:41 -0700

    update mqtt file used for debugging

--- src/mqtt.mjs
+++ src/mqtt.mjs
@@ -1,12 +1,22 @@
 import mqtt from 'mqtt';
 import protobuf from 'protobufjs';
+import config from 'config';
 
 // Load a .proto file
 const root = protobuf.loadSync('/Users/isamuito/Documents/TOKU/sparkplugb/src/sample.proto');
 
-const client = mqtt.connect('mqtt://localhost:1883');
+const username = config.get('username');
+const password = config.get('password');
+
+const client = mqtt.connect({
+  protocol: "tcp",
+  host: "localhost",
+  port: 1883,
+  username: username,
+  password: password,
+});
 client.on("connect", () => {
-  client.subscribe("#", (err) => {
+  client.subscribe("spBv1.0/Sparkplug/#", (err) => {
     if (!err) {
       // client.publish("presence/test/test", "Hello mqtt");
       console.log("#");
@@ -18,8 +28,8 @@ client.on("message", (topic, message) => {
   // message is Buffer
   console.log(topic.toString());
   // process message protobuf
-  // const messageProto = root.lookupType("Payload");
-  // const decodedMessage = messageProto.decode(message);
-  // console.log(decodedMessage);
+  const messageProto = root.lookupType("Payload");
+  const decodedMessage = messageProto.decode(message);
+  console.log(decodedMessage.timestamp);
   // client.end();
 });
